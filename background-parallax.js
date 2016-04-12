775a0e53801f194a488f4e3792450b56e023821a
Juned Chhipa <juned@ourphilosophy.co.uk>
2016-04-12 21:36:57 +0530

    chrome fix

--- background-parallax.js
+++ background-parallax.js
@@ -1,6 +1,6 @@
 var background_parallax = function(el, speed) {
 	// elements top y position relative to viewport
-	var scrollY = el.getBoundingClientRect().y;
+	var scrollY = el.getBoundingClientRect().top;
 	// el's height including paddings/borders
 	var elHeight = el.offsetHeight;
 
